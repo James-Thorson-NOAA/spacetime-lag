d9e6c9021dd4c67e5d2a0d9cfc6dad8a9da6a661
Max Lindmark <max.lindmark@slu.se>
2026-01-05 15:36:20 +0100

    modify figures to match Jim's

--- plot-selfcross.R
+++ plot-selfcross.R
@@ -5,6 +5,7 @@ library(dplyr)
 library(stringr)
 library(readr)
 library(ggplot2)
+library(RColorBrewer)
 library(ggsidekick); theme_set(theme_sleek())
 
 root_dir <- here::here()
@@ -13,37 +14,44 @@ root_dir <- here::here()
 n_sim <- 100
 
 res_df <- read_csv(file.path(root_dir, paste0("2025-11-09/simulation_selfcross_out_", n_sim, "_reps.csv")))
+#res_df <- read_csv(file.path(root_dir, paste0("2025-11-09/simulation_selfcross_out_", n_sim, "_reps.csv")))
 
 # specify true model for each species
 correct_models <- tibble(
   species = c("capelin", "pacific cod", "pacific halibut"),
-  correct_type = c("space+time", "base", "time")
+  correct_type = c("ST", "null", "T")
 )
 
 res_df |> 
   distinct(AIC, type, iter, species) |> 
   mutate(type = case_when(
-    type == "1-000" ~ "base",
-    type == "1-100" ~ "space",
-    type == "1-010" ~ "time",
-    type == "1-110" ~ "space+time",
+    type == "1-000" ~ "null",
+    type == "1-100" ~ "S",
+    type == "1-010" ~ "T",
+    type == "1-110" ~ "ST",
     TRUE ~ type
   )) |>
+  mutate(
+    type = factor(type, levels = c("null", "S", "T", "ST"))
+  ) |> 
   mutate(delta_AIC = AIC - min(AIC), .by = c(iter, species)) |> 
   summarise(fraction_best = mean(delta_AIC == 0), .by = c(type, species)) |>
   left_join(correct_models, by = "species") |>
   mutate(true_model = type == correct_type) |>
-  ggplot(aes(fraction_best, type, fill = true_model)) +
-  facet_wrap(~str_to_sentence(species), ncol = 3) +
-  theme(legend.position = "bottom",
+  ggplot(aes(type, fraction_best, fill = true_model)) +
+  facet_wrap(~species, ncol = 3) +
+  guides(fill = guide_legend(position = "inside")) +
+  theme(legend.position.inside = c(0.1, 0.8),
   panel.spacing = unit(1, "lines"),
         plot.margin = margin(5, 10, 5, 5)) +
+  coord_cartesian(expand = 0, ylim = c(0,1)) +
   geom_col(width = 0.9) +
+  scale_y_continuous(breaks = seq(0, 1, 0.1)) +
   labs(
-    y = "Model",
-    x = "Fraction of iterations with best AIC"
+    x = "model",
+    y = "fraction of iterations with best AIC"
   ) +
-  scale_fill_brewer(palette = "Set2", name = "Operating model",
+  scale_fill_brewer(palette = "Set2", name = "operating model",
                     direction = -1)
 
 ggsave(paste0(root_dir, "/figs/self_test_aic.png"), width = 20, height = 8, unit = "cm")
@@ -81,10 +89,10 @@ pdat <- res_df |>
   ) |> 
   # rename the model types
   mutate(type = case_when(
-    type == "1-000" ~ "base",
-    type == "1-100" ~ "space",
-    type == "1-010" ~ "time",
-    type == "1-110" ~ "space+time",
+    type == "1-000" ~ "null",
+    type == "1-100" ~ "S",
+    type == "1-010" ~ "T",
+    type == "1-110" ~ "ST",
     TRUE ~ type
   )) |>
   left_join(correct_models, by = "species") |>
@@ -100,11 +108,11 @@ pdattrue <- pdat |>
 # Filter estimated parameters
 pdathat <- pdat |> 
   filter(type_of_par == "par_hat") |> 
-  mutate(true_model = ifelse(true_model == TRUE, "AIC-selected", "Not AIC-selected")) 
+  mutate(true_model = ifelse(true_model == TRUE, "TRUE", "FALSE")) 
 
 # Add in rmsd (for making different symbols)
 rmsd <- pdathat |> 
-  filter(par_name == "RMSD" & type %in% c("space+time", "space")) |> 
+  filter(par_name == "RMSD" & type %in% c("ST", "S")) |> 
   dplyr::select(iter, type, species, value) |> 
   rename(RMSD = value)
 
@@ -126,9 +134,7 @@ pdathat <- pdathat |>
                                     "kappaT_pacific halibut",
                                     "RMSD_capelin", 
                                     "AR(1)_capelin", 
-                                    "AR(1)_pacific halibut"))
-  ) |> 
-  mutate(species = str_to_sentence(species))
+                                    "AR(1)_pacific halibut")))
 
 # Do the same for pdattrue
 pdattrue <- pdattrue |> 
@@ -144,6 +150,8 @@ pdattrue <- pdattrue |>
                                     "AR(1)_pacific halibut"))
   )
 
+legend_col <- brewer.pal(3, "Dark2")[1:2]
+
 p1 <- pdathat |> 
   ggplot(aes(type, value)) +
   facet_wrap(~facet_order, 
@@ -153,32 +161,34 @@ p1 <- pdathat |>
                c("log_kappaS_capelin" = "log(kappa[S])",
                  "kappaT_capelin" = "kappa[T]",
                  "kappaT_pacific halibut" = "kappa[T]",
-                 "RMSD_capelin" = "RMSD",
-                 "AR(1)_capelin" = "AR(1)",
-                 "AR(1)_pacific halibut" = "AR(1)"),
+                 #"RMSD_capelin" = "atop(Root-mean~squared, displacement~(km))",
+                 "RMSD_capelin" = "Root*'\u2013'*mean*'\u2013'*squared~displacement~(km)",
+                 "AR(1)_capelin" = "rho[t]",
+                 "AR(1)_pacific halibut" = "rho[t]"),
                default = label_parsed
-             ))) +
+             ))
+             ) +
   geom_hline(data = pdattrue, aes(yintercept = value), linetype = 2,
              color = "tomato", alpha = 0.7, linewidth = 0.6) +
   geom_quasirandom(aes(fill = true_model, shape = shape_value),
-                   alpha = 0.8, size = 1, color = "gray70") +
-  #scale_linetype_manual(values = c(3, 2, 1), name = "True value") +
+                   alpha = 0.4, size = 1.5, color = "gray95") +
   scale_shape_manual(values = c(4, 21), name = "") +
-  scale_fill_manual(values = c("gray50", "gray99"), name = "") +
+  scale_fill_brewer(palette = "Dark2", name = "operating model", direction = -1) +
   geom_boxplot(fill = NA, width = 0.2, size = 0.4,
                outlier.shape = NA, color = "gray20") +
   geom_text(data = . %>% distinct(species, facet_order),
             aes(label = species, x = Inf, y = Inf),
-            hjust = 1.1, vjust = 1.2, size = 3, color = "gray30") +
-  labs(y = "Estimated value",
-       x = "Estimation model") +
+            hjust = 1.1, vjust = 1.5, size = 3, color = "gray30") +
+  labs(y = "estimated value",
+       x = "estimation model") +
   theme(legend.position = "bottom",
-        legend.key.width = unit(0.5, "cm")) +
-  guides(fill = guide_legend(ncol = 2, override.aes = list(color = c("gray20", "gray90"))),
-         shape = guide_legend(ncol = 2),
+        legend.key.width = unit(0.5, "cm"),
+        strip.text.x.top = element_text(size = 9.5)) +
+  guides(fill = guide_legend(ncol = 2, override.aes = list(color = rev(legend_col))),
+         shape = guide_legend(ncol = 2, override.aes = list(color = "gray10")),
          linetype = guide_legend(title.position = "top", title.hjust = 0.5))
 
-tag_facet(p1, fontface = 1, open = "", close = ")", color = "gray20")
+tag_facet(p1, fontface = 1, open = "", close = ")", color = "gray20", label = LETTERS[1:6])
 
 ggsave(paste0(root_dir, "/figs/kappa_recovery.png"), width = 22, height = 17, unit = "cm")
 
